d54681eff060a9b7d0a71e67ce9f4c269c2370c2
arman <armaanepiic@gmail.com>
2026-02-08 03:18:39 +0600

    function input-output

--- first-project/main.go
+++ first-project/main.go
@@ -1,61 +1,35 @@
 package main
+
 import "fmt"
 
+// print a value
+func add (num1 int, num2 int) {
+	sum := num1 + num2
+	fmt.Println(sum)
+}
+
+// return value
+func findProduct (num1 int, num2 int) int {
+	result := num1 * num2
+	return result
+}
+
+func getNumbers (num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	product := num1 * num2
+	return sum , product
+}
 func main() {
-	// integer
-	var x int = 100
-	var y = 200
-	z := 300
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
 
-	// float
-	var a float32 = 23.34
-	fmt.Println(a)
+	// function
+	var a int = 10
+	b := 20
+	add (a, b)
+	product := findProduct(a, b)
+	fmt.Println(product)
 
-	// string
-	message := "I love you"
-	fmt.Println(message)
+	p, q := getNumbers(a, b)
+	fmt.Println(p)
+	fmt.Println(q)
 
-	// boolean
-	isOk := true
-	fmt.Println(isOk)
-	isOk = false
-	fmt.Println(isOk)
-	// isOk = "Hello" => error because at first declaration isOk was boolean
-
-	// const
-	const pi = 3.1416
-	fmt.Println(pi)
-
-	// condition
-	age := 20
-	sex := "male"
-
-	if(age > 60 || sex == "male") {
-		fmt.Println("You are ready to marry")
-	}
-
-	isPretty := false
-	if (!isPretty) {
-		fmt.Println("You are pretty")
-	}
-
-	day := "monday"
-
-	switch day {
-	case "friday" , "saturday": fmt.Println("Holiday")
-	case "monday" : fmt.Println("half office day")
-	default:
-		fmt.Printf("Office day")
-	}
-
-	// if(age > 18) {
-	// 	fmt.Println("You are ready for marriage")
-	// } else if (age == 18) {
-	// 	fmt.Println("you are just 18")
-	// } else {
-	// 	fmt.Println("you are teenager")
-	// }
 }
