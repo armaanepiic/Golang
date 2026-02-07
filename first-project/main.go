10b770c8c9325080128b0d1be5486dab21077afb
arman <armaanepiic@gmail.com>
2026-02-07 15:52:50 +0600

    variable declaration

--- first-project/main.go
+++ first-project/main.go
@@ -2,6 +2,30 @@ package main
 import "fmt"
 
 func main() {
-	a:=100
+	// integer
+	var x int = 100
+	var y = 200
+	z := 300
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	// float
+	var a float32 = 23.34
 	fmt.Println(a)
+
+	// string
+	message := "I love you"
+	fmt.Println(message)
+
+	// boolean
+	isOk := true
+	fmt.Println(isOk)
+	isOk = false
+	fmt.Println(isOk)
+	// isOk = "Hello" => error because at first declaration isOk was boolean
+
+	// const
+	const pi = 3.1416
+	fmt.Println(pi)
 }
