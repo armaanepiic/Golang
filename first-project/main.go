bb8f98065248a2e44463b2c701fced9b79a3f9c4
arman <armaanepiic@gmail.com>
2026-02-17 08:12:32 +0600

    feat: variable shadowing

--- first-project/main.go
+++ first-project/main.go
@@ -1,25 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-var (
-	a = 10
-	b = 20
-)
+var a = 10
 
-func add (x int, y int) {
-	res := x + y
-	printNum(res)
-} 
-
-func main() { 
-	add(a, b)
+func main() {
+	age := 30
+	if age > 18 {
+		a := 47
+		fmt.Println(a)
+	}
+	fmt.Println(a)
 }
-
-func printNum(num int) {
-	fmt.Println(num)
-}
-// when a function is called a memory will be allocated in the RAM
-// after finishing the execution the memory allocated will be gone
