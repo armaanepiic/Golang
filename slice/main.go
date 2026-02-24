001047e9d6363f6370b7ac078c50553b6b0a84e7
arman <armaanepiic@gmail.com>
2026-02-25 03:46:48 +0600

    feat: slice method

--- slice/main.go
+++ slice/main.go
@@ -2,7 +2,52 @@ package main
 
 import "fmt"
 
+func changeSlice(p []int) []int {
+	p[0] = 10
+	p = append(p, 11)
+	return p
+}
+
 func main() {
+
+	// var x []int 		// [], len=0, cap=0
+	// x = append(x, 1)	// [1], len=1, cap=1
+	// x = append(x, 2)
+	// x = append(x, 3)
+	// // fmt.Println(x)  
+
+	// y := x
+	// x = append(x, 4)
+	// y = append(y, 5)
+	// x[0] = 10
+	// fmt.Println(x) // [10 2 3 5]
+	// fmt.Println(y) // [10 2 3 5]
+
+
+	x := []int{1, 2, 3, 4, 5}
+
+	x = append(x, 6)
+	x = append(x, 7)
+
+	a := x[4:]
+
+	y := changeSlice(a)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(x[0:8])
+
+
+
+
+
+
+
+
+
+
+
+
 	// arr := [6]string{"This", "is", "a", "go", "interview", "course"}
 	// fmt.Println(arr)
 
@@ -31,9 +76,9 @@ func main() {
 	// fmt.Println("slice:",s, "len:", len(s), "cap:", cap(s))
 
 	// empty slice or nil slice
-	var s []int // len=0, cap=0
-	s = append(s, 1, 2, 3)
-	fmt.Println(s)
+	// var s []int // len=0, cap=0
+	// s = append(s, 1, 2, 3)
+	// fmt.Println(s)
 }
 
 /*
@@ -45,6 +90,9 @@ func main() {
 		4. make func with len					=> s := make([]int, 3) 
 		5. make func with len and capacity		=> s := make([]int, 3, 5) 
 		6. emply slice / nil slice				=> var s []int
+	
+	rule of expanding space: slice underlying array rule => till 1024 (100% increase)
+	after 1024 it will increase by 25%
 */
 
 
