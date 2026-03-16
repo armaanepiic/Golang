051276b7d26f0b932b3f9532540699fd77a25a12
arman <armaanepiic@gmail.com>
2026-03-17 04:05:10 +0600

    feat: defer method

---
+++ defer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func a() {
+	i := 0
+
+	fmt.Println("first", i)
+
+	defer fmt.Println("second", i)
+
+	i = i + 1
+	
+	fmt.Println("third", i)
+	
+	defer fmt.Println("fourth", i)
+	
+
+}
+
+func main() {
+	a()
+}
