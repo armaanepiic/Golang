ccf1f2a61d612f0fa92dec8fbed59317e2630d11
arman <armaanepiic@gmail.com>
2026-03-30 21:29:16 +0600

    all the practice code is in new branch

---
+++ goroutine/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var a = 10
+const p = 11
+
+func printHello(num int) {
+	fmt.Println("Hello Habib", num)
+}
+
+func main() {
+	go printHello(1)
+	go printHello(2)
+	go printHello(3)
+	go printHello(4)
+	go printHello(5)
+
+	fmt.Println(a, " ", p)
+
+	time.Sleep(5 * time.Second)
+}
+
+/*
+
+** code segment **
+
+p = 11
+printHello = printHello(num int){...}
+main = main(){...}
+
+*/
